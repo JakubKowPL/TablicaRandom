104751c7a6f915ec1c3c76eb52df45bac348c819
Jakub Kowalczyk <jakubkowalczyk@example.com>
2023-03-03 14:30:52 +0100

    Test pusha z Idei.

--- src/pl/jakubkow/nauka/dziedziczenie/Main.java
+++ src/pl/jakubkow/nauka/dziedziczenie/Main.java
@@ -2,7 +2,7 @@ package pl.jakubkow.nauka.dziedziczenie;
 
 class TablicaRandom {
     public static void main(String[] args) {
-        int tablica[] = new int[100];
+        int[] tablica = new int[100];
         for (int i = 0; i < tablica.length; i++)
             tablica[i] = (int) (Math.random() * 1001); //picks random values <0;1000>
         int a, b, t;
