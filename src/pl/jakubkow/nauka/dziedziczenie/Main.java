5b14b7c68f761721bb10c648db8029c1ab2b3dbe
Jakub Kowalczyk <jakubkowalczyk@example.com>
2023-03-03 14:28:25 +0100

    Uzupełnienie deklaracji pakietu.

--- src/pl/jakubkow/nauka/dziedziczenie/Main.java
+++ src/pl/jakubkow/nauka/dziedziczenie/Main.java
@@ -1,5 +1,7 @@
+package pl.jakubkow.nauka.dziedziczenie;
+
 class TablicaRandom {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int tablica[] = new int[100];
         for (int i = 0; i < tablica.length; i++)
             tablica[i] = (int) (Math.random() * 1001); //picks random values <0;1000>
